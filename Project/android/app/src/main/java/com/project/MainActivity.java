0df5c49c0df8d574a2da76af4e2caa765bfa3021
Manin <manin.t@ku.th>
2019-11-26 16:04:32 +0700

    install map sdk for android

--- Project/android/app/src/main/java/com/project/MainActivity.java
+++ Project/android/app/src/main/java/com/project/MainActivity.java
@@ -1,6 +1,16 @@
 package com.project;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -8,8 +18,74 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  private MapView mapView;
+
   @Override
   protected String getMainComponentName() {
     return "Project";
   }
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    Mapbox.getInstance(this, "YOUR_MAPBOX_ACCESS_TOKEN");
+    setContentView(R.layout.activity_main);
+    mapView = findViewById(R.id.mapView);
+    mapView.onCreate(savedInstanceState);
+    mapView.getMapAsync(new OnMapReadyCallback() {
+      @Override
+      public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+          @Override
+          public void onStyleLoaded(@NonNull Style style) {
+
+          // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+
+          }
+        });
+      }
+    });
+  }
+  @Override
+  public void onStart() {
+    super.onStart();
+    mapView.onStart();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    mapView.onResume();
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    mapView.onPause();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    mapView.onStop();
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
+
 }
